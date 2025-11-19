b127dbac0685045214bb93e11f19855a82e51e2b
Ruiwen Zhao <zhaoruiwen@baizhenbio.cn>
2025-11-19 12:53:24 +0800

    57: delete all files for a complete rewrite

--- README.md
+++
@@ -1,2 +0,0 @@
-# Spotease
-Convert your spotify playlist to netease music and back in real-time
--- src/app/api/auth/spotify/route.ts
+++
@@ -1,7 +0,0 @@
-import { getSpotifyAuthUrl } from "@/lib/spotify";
-import { redirect } from "next/navigation";
-
-export async function GET() {
-  const authUrl = getSpotifyAuthUrl();
-  redirect(authUrl);
-}
