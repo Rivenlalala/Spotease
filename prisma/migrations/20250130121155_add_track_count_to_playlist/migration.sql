c9622d727be43a6a54cf8d80fc508dede4ff197e
rivenlalala <riven012303@gmail.com>
2025-12-28 00:20:00 +0800

    chore: remove old files for a complete rework

--- prisma/migrations/20250130121155_add_track_count_to_playlist/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `trackCount` to the `Playlist` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Playlist" ADD COLUMN     "trackCount" INTEGER NOT NULL;
